cb4691e7768ff8256c068d97cd95c761f4cf80fc
thiagossmarins <thiagodesouzasallesmarins@gmail.com>
2024-08-21 20:45:22 -0300

    aprendizado

---
+++ banco-relacional/atualizarEstados.sql
@@ -0,0 +1,11 @@
+UPDATE estados
+set nome = 'Maranhão'
+WHERE sigla = 'MA'
+
+select nome from estados where sigla = 'MA'
+
+UPDATE estados
+set nome = 'Paraná'
+where sigla = 'PR'
+
+select nome from estados where sigla = 'PR' 
